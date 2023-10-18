e07ee9bd452f7975dd894fc2f173f7ea3ad4a9d0
yosepb <145565073+yosepb@users.noreply.github.com>
2023-10-18 07:26:59 +0700

    Add files via upload

---
+++ Assets/Code/ButtonManagerScan.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManagerScan : MonoBehaviour
+{
+    public void LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
---
+++ Assets/Code/ButtonManagerScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonManagerScript : MonoBehaviour
+{
+    public GameObject home;
+    public GameObject tutorial;
+    public GameObject tentang;
+
+    void Start()
+    {
+        home.SetActive(true);
+        tutorial.SetActive(false);
+        tentang.SetActive(false);
+    }
+
+    public void homeClicked()
+    {
+        home.SetActive(true);
+        tutorial.SetActive(false);
+        tentang.SetActive(false);
+    }
+
+    public void tutorialClicked()
+    {
+        home.SetActive(false);
+        tutorial.SetActive(true);
+        tentang.SetActive(false);
+    }
+
+    public void tentangClicked()
+    {
+        home.SetActive(false);
+        tutorial.SetActive(false);
+        tentang.SetActive(true);
+    }
+
+    public void LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+    public void exitClicked()
+    {
+        Application.Quit();
+    }
+}
